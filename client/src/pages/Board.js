bb97c4a5eed22e30eb97b73b5ab3f1f3ba35aaa6
Rachel Harris <rachel.harris@msn.com>
2022-05-16 10:35:57 +0800

    Dispaly only relevant cards in each section of the board

--- client/src/pages/Board.js
+++ client/src/pages/Board.js
@@ -9,6 +9,24 @@ import { QUERY_ITEMS } from '../utils/queries';
 const Board = () => {
   const { data } = useQuery(QUERY_ITEMS);
   const items = data?.items || [];
+  
+  function filterCat01() {
+    return items.filter(
+      (items) => items.category === "01"
+    )
+  }
+
+  function filterCat02() {
+    return items.filter(
+      (items) => items.category === "02"
+    )
+  }
+
+  function filterCat03() {
+    return items.filter(
+      (items) => items.category === "03"
+    )
+  }
 
     return (
       <main className="board-container">
@@ -21,8 +39,7 @@ const Board = () => {
               <h3 className='section-heading'>What went well?</h3>
               <div className='section-area'>
                 <ul>
-                  {items &&
-                    items.map((item) => (
+                  {filterCat01().map((item) => (
                     <li key={item.id}>{item.text}</li>
                   ))}
                 </ul>
@@ -31,11 +48,21 @@ const Board = () => {
           <div className='section'>
               <h3 className='section-heading'>What didn't go well?</h3>
               <div className='section-area'>
+              <ul>
+                  {filterCat02().map((item) => (
+                    <li key={item.id}>{item.text}</li>
+                  ))}
+                </ul>
               </div>
           </div>
           <div className='section'>
               <h3 className='section-heading'>What confuses you?</h3>
               <div className='section-area'>
+                <ul>
+                  {filterCat03().map((item) => (
+                    <li key={item.id}>{item.text}</li>
+                  ))}
+                </ul>
               </div>
           </div>
         </div> 
