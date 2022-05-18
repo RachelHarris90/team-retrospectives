c7794e029ff89cb3e4d20ebba78971ae7bdacba6
Rachel Harris <rachel.harris@msn.com>
2022-05-18 17:29:21 +0800

    Fix connection to mongo

--- server/config/connection.js
+++ server/config/connection.js
@@ -4,7 +4,7 @@ require('dotenv').config();
 mongoose.connect(
   //TODO: Swap around when deploying (process.env.MongoDB first.. )
    //'mongodb://localhost:27017/retroDB' || process.env.MONGODB_URI, 
-    process.env.MONGODB_URI || 'mongodb://localhost:27017/retroDB',
+    process.env.MONGODB_URI,
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
