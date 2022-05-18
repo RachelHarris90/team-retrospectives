a95babda1bab83ac89850fc05cb4385f9a664c35
Rachel Harris <rachel.harris@msn.com>
2022-05-18 15:24:31 +0800

    Deploy to test locally

--- server/config/connection.js
+++ server/config/connection.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost:27017/retroDB', 
+  'mongodb://localhost:27017/retroDB' || process.env.MONGODB_URI, 
+  // process.env.MONGODB_URI || 'mongodb://localhost:27017/retroDB', 
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
