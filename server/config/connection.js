0f1cdcd9fe7d534b2039fcaadf9eca351bd82e0f
Rachel Harris <rachel.harris@msn.com>
2022-05-18 14:00:22 +0800

    Update config for heroku deployment

--- server/config/connection.js
+++ server/config/connection.js
@@ -2,8 +2,7 @@ const mongoose = require('mongoose');
 require('dotenv').config();
 
 mongoose.connect(
-  //TODO: Swap around when deploying (process.env.MongoDB first.. )
-   'mongodb://localhost:27017/retroDB' || process.env.MONGODB_URI, 
+  process.env.MONGODB_URI || 'mongodb://localhost:27017/retroDB', 
   {
     useNewUrlParser: true,
     useUnifiedTopology: true,
